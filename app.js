9d70a0a84625761833ab7e49fa7900ac38805d9f
Himanshu <himanshudhawale9@gmail.com>
2021-09-13 17:59:36 +0530

    updated output

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const luckyNumber = document.querySelector("#lucky-number");
 const checkNumberButton = document.querySelector("#check-number");
 const outputBox = document.querySelector("#output-box");
 
+function checkBirthDateIsLucky(){
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    if(sum&&dob&&luckyNumber.value)
+    compareValues(sum, luckyNumber.value);
+    else
+    outputBox.innerText ="Please Enter Both The Fields😓";
+} 
+
+
+
 function compareValues (sum, luckyNumber){
     if(sum%luckyNumber===0){
         outputBox.innerText = "Your Birthday is Lucky🚀";
@@ -12,17 +23,10 @@ function compareValues (sum, luckyNumber){
     }
 }
 
-function checkBirthDateIsLucky(){
-    const dob = dateOfBirth.value;
-    const sum = calculateSum(dob);
-    if(sum&&dob)
-    compareValues(sum, luckyNumber.value);
-    else
-    outputBox.innerText ="Please Enter Both The Fields😓";
-} 
+
 
 function calculateSum(dob){
-    dob = dob.replaceAll("-","");
+    dob = dob.replaceAll("-","0");
     let sum=0;
     for(let index=0; index<dob.length; index++){
         sum = sum + Number(dob.charAt(index));
